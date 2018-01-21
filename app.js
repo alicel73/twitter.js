6ef41c2cb7eff011bd5dd94faa5f5c831b7a20dd
Alice Luong <aliceluong73@gmail.com>
2018-01-21 17:03:35 -0500

    first step

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => res.send('WELCOME!'));
+
+
+app.listen(3000, () => console.log('server listening'));
